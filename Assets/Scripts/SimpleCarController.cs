bf92dd1c91f55e92282ce3069a54640340af7cd0
DESKTOP-3PT05KH\Lukas <lukas.ngo@students.plymouth.ac.uk>
2017-10-26 22:09:23 +0100

    update 26/10/17 testing
    removed stuff from other projects, added car script, terrain, split screen, and track

---
+++ Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class SimpleCarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 oldRot = transform.rotation.eulerAngles;
+
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                GetComponent<Rigidbody>().drag = 0.3f;
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            if (axleInfo.motor && Input.GetAxis("Vertical") <= 0)
+            {
+                GetComponent<Rigidbody>().drag = 3f;
+            }         
+
+            transform.rotation = Quaternion.Euler(oldRot.x, oldRot.y, 0);
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
